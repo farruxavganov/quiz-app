5026c0ab5967ae7a762d289d1e973cf907305169
farrux <farrux.avganov@mail.ru>
2022-06-17 18:41:06 +0300

    friday bigen

---
+++ app.js
@@ -0,0 +1,11 @@
+const questionArry = [{},{},{},{}];
+
+const question = document.getElementById("question");
+const input_a = document.getElementById("text_a");
+const input_b = document.getElementById("text_b");
+const input_c = document.getElementById("text_c");
+const input_d = document.getElementById("text_d");
+const submit = document.querySelector(".btn");
+
+let countQuistionIndex = 0;
+
